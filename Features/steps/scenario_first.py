84176210d3f5ed9a9cd749d9620be61361149d04
Chutchev <iChutchev98@gmail.com>
2018-10-23 14:08:08 +0500

    Added: Make screenshots for some steps

--- Features/steps/scenario_first.py
+++ Features/steps/scenario_first.py
@@ -4,26 +4,26 @@ import time
 from datetime import datetime
 from selenium.common.exceptions import *
 import os
+from PIL import ImageGrab
 
 
 @when("Считать имя канала из текстового файла channel_name.txt")
 def step_impl(context):
-    with open(os.path.realpath("channel_name.txt"), "r") as channel_name_file:
+    with open(os.path.abspath("channel_name.txt"), "r") as channel_name_file:
         context.channel_name = channel_name_file.readline()
 
 
 @step("Зайти на этот канал на твиче")
 def step_impl(context):
-    context.driver = webdriver.Chrome(os.path.realpath("chromedriver.exe"))
     context.driver.get("http:\\twitch.tv\\" + context.channel_name)
 
 
-@step("Сделать полноэкрнный режим")
+@step("Сделать полноэкранный режим")
 def step_impl(context):
     context.driver.maximize_window()
 
 
-@then('Просмотреть стрим "{second}" секунд')
+@then('Просмотреть стрим "{second}" секунд, в случае ошибки, сделать скриншот браузера')
 def step_impl(context, second):
     try:
         try:
@@ -32,7 +32,9 @@ def step_impl(context, second):
             but_play = context.driver.find_element_by_xpath('//*[@class="player-content-button js-player-matur'
                                                         'e-accept js-mature-accept-label"]').click()
     except NoSuchElementException:
-        skip_scenario(context, context.result)
+        screen = ImageGrab.grab()
+        screen.save("Screenshots\\not_find_a_channel.jpg")
+        skip_scenario(context)
     time.sleep(int(second))
 
 
@@ -41,20 +43,19 @@ def step_impl(context, tab_name):
     need_tab = context.driver.find_element_by_xpath((('//*[@class="tw-font-size-5" and contains(text(), '
                                                       '"{}")]').format(tab_name))).click()
 
-
-@then('Просмотреть содержимое вкладки в течении "{second}" секунд')
-def step_impl(context, second):
+@then('Просмотреть содержимое вкладки "{tab_name}" в течении "{second}" секунд и сделать скриншот')
+def step_impl(context, second, tab_name):
     time.sleep(int(second))
-
+    screen = ImageGrab.grab()
+    screen.save("Screenshots\\Вкладка {}.jpg".format(tab_name))
 
 @step('Записать в логи результат')
 def step_impl(context):
-    with open(os.path.realpath("log.txt"), "a") as log:
+    with open(os.path.abspath("log.txt"), "a") as log:
         log.write("{0} Сценарий выполнен.\n".format(datetime.now()))
 
 
-def skip_scenario(context, result):
-    if result == "Неудачно":
-        with open("C:\\Users\\Ivan\\PycharmProjects\\untitled3\\Features\\steps\\log.txt", "a") as log:
-            log.write("{0} Сценарий не выполнен.\n".format(datetime.now()))
-            context.scenario.skip(require_not_executed=True)
+def skip_scenario(context):
+    with open(os.path.abspath("log.txt"), "a") as log:
+        log.write("{0} Сценарий не выполнен.\n".format(datetime.now()))
+        context.scenario.skip(require_not_executed=True)
