f4708e99a00f94b16460a45a825614139c35df1d
chutchev <chutchev>
2018-10-27 13:56:21 +0500

    Проведен рефакторинг кода

--- Features/environment.py
+++ Features/environment.py
@@ -1,14 +1,19 @@
 import behave
+import shutil
 import os
 from selenium import webdriver
 
 def before_all(context):
-    context.driver = webdriver.Chrome(os.path.abspath("chromedriver.exe"))
-    dirs = os.listdir()
-    create = True
-    for index in range(len(dirs)):
-        if dirs[index] == "Screenshots":
-            create == False
-            return
-    if create == True:
+    if os.path.exists(os.path.abspath(".\\Screenshots")):
+        shutil.rmtree(os.path.abspath(".\\Screenshots"))
+    else:
         os.makedirs(".\\Screenshots\\")
+    context.driver = webdriver.Chrome(os.path.abspath("chromedriver.exe"))
+    context.driver.maximize_window()
+
+
+def after_all(context):
+    shutil.rmtree(os.path.abspath(".\\Screenshots"))
+    os.remove(os.path.abspath(".\\Screenshots.zip"))
+
+
